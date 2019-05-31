87a5c3b4c4e6fe6b40e3cccfc57da5989b12378d
Jeff Howarth <jeffhowarth31@gmail.com>
2019-05-31 14:36:47 -0700

    3rd commit

--- src/app/home/create-account/create-account.component.ts
+++ src/app/home/create-account/create-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../shared/users.service';
 
 @Component({
   selector: 'app-create-account',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: UsersService) { }
 
   ngOnInit() {
+  }
+
+  onCreateUser(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    
+    this.service.addUser(form.value.firstName, form.value.lastName, form.value.username, form.value.password, form.value.email);
+    
+    form.resetForm();
   }
 
 }
--- src/app/home/shared/users.service.ts
+++ src/app/home/shared/users.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { User } from 'backend/models/user';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private users: User[] = [];
+  private usersUpdated = new Subject<User []>();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addUser(firstName: string, lastName: string, username: string, password: string, email: string) {
+    const user: User = { id: null, firstName, lastName, username, password, email };
+    this.http.post<{ message: string, userId: string }>('http://localhost:3000/api/users', user);
+  }
 }
